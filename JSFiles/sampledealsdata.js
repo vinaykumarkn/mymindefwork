a458ec43b56ea8a450804b0b906c9632fb3a8dbb
V!N@yKuM@r <vinaykn.ttp@gmail.com>
2017-01-03 14:13:12 +0530

    testing

---
+++ JSFiles/sampledealsdata.js
@@ -0,0 +1,3 @@
+[{"Title":"Universal Studio Singapore Promotion: Winter Season","PromoPeriod":"01 Dec 2016 to 02 Jan 2017","PostedBy":"MSS-PSC","PostedDate":"29-12-2016 5:08pm"},
+{"Title":"Financial Planning Awareness Talk","PromoPeriod":"25 Oct 2016 to 04 Nov 2016","PostedBy":"MSS-PSC","PostedDate":"29-12-2016 5:08pm"},
+{"Title":"Holiday Promotion: Mandarin, Singapore (Applicable for Fridays to Sundays, Eve of Public Holidays and Public Holidays)","PromoPeriod":"28 Oct 2016 to 31 Mar 2017","PostedBy":"MSS-PSC","PostedDate":"29-12-2016 5:08pm"}]
